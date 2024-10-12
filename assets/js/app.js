f1546085209545d371166e9acc482946753b1758
Sara Vergara <sarasilvia.vergara@gmail.com>
2024-10-11 21:30:21 -0600

    feat: revisión y corrección de errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const usersEndpoint = `${baseEndpoint}/users`; 
+const n = document.querySelector('.name');  // Se eliminó el signo '$' a las variables const por buenas práticas & al "name" se le agregó un . porque en el html esta calificado como una class
+const b = document.querySelector('.blog'); // blog es una class, se agrega "."
+const l = document.querySelector('.location'); // se agrega un . por ser una class
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+const displayUser = async (username) => { // Se reestructura a funcion a una arrow function
+  n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);  // El await solo es válido en una async function por eso tenemos que reestructurar
+  const user = await response.json(); // se utiizó el método JSON para connversión a formato json
+  console.log(user.data); // Se agrega la constante user
+  n.textContent = '${data.name}'; 
+  b.textContent = '${data.blog}';
+  l.textContent = '${data.location}';
 }
 
-function handleError(err) {
+const handleError = (err) => { // también se reestructura a una arrow function
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`; // se escribe ';' al final de la línea de código
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinsk').catch(handleError(n)); // se agregó n 
